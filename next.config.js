051ba1866317a3db23573dd0e8dea27ebda65913
jared <jared.hembrow@gmail.com>
2023-09-30 15:04:58 +0300

    added basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+    basePath: isProd ? "/portfolio" : "",
     output: 'export',
     distDir: 'dist',
     images: {
